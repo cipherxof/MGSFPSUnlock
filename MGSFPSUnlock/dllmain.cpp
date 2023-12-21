2a671782be7b899f81ff87673e5c8cfaaad15533
trigger <triggerhappy187@live.com>
2023-12-20 16:37:18 -0800

    update for game version 1.4.0

--- MGSFPSUnlock/dllmain.cpp
+++ MGSFPSUnlock/dllmain.cpp
@@ -28,9 +28,9 @@ GetTargetFpsDelegate* GetTargetFps;
 ThrowItemDelegate* ThrowItem;
 UpdateAnimationBlendingDelegate* UpdateAnimationBlending;
 
-bool InCutscene() // todo: find a more accurate method
+bool InCutscene()
 {
-    return CutsceneFlag == NULL ? false : *CutsceneFlag == 0;
+    return CutsceneFlag == NULL ? false : *CutsceneFlag == 1;
 }
 
 uint64_t __fastcall GetTimeBaseHook(struct _exception* a1)
@@ -90,10 +90,10 @@ void InstallHooks()
 {
     int status = MH_Initialize();
 
-    uintptr_t getTimeBaseOffset = (uintptr_t)Memory::PatternScan(GameModule, "48 83 EC 28 E8 97 E6 FD FF 33 C9 83 F8 01 0F 94");
+    uintptr_t getTimeBaseOffset = (uintptr_t)Memory::PatternScan(GameModule, "48 83 EC 28 E8 ?? ?? FD FF 33 C9 83 F8 01 0F 94");
     uintptr_t updateMotionAOffset = (uintptr_t)Memory::PatternScan(GameModule, "48 85 C9 74 31 0F 57 C0 0F 2F C1 76 0B 66 0F 6E");
     uintptr_t updateMotionBOffset = (uintptr_t)Memory::PatternScan(GameModule, "48 85 C9 74 3F 0F 57 C0 0F 2F C1 76 0B 66 0F 6E");
-    uintptr_t getTargetFpsOffset = (uintptr_t)Memory::PatternScan(GameModule, "48 83 EC 28 E8 77 E6 FD FF 83 F8 01 B9 3C 00 00");
+    uintptr_t getTargetFpsOffset = (uintptr_t)Memory::PatternScan(GameModule, "48 83 EC 28 E8 ?? ?? FD FF 83 F8 01 B9 3C 00 00");
     uintptr_t throwItemOffset = (uintptr_t)Memory::PatternScan(GameModule, "40 55 56 57 41 56 41 57 48 8D 6C 24 E0 48 81 EC");
     uintptr_t animBlendOffset = (uintptr_t)Memory::PatternScan(GameModule, "48 89 5C 24 08 57 48 83  EC 30 0F 29 74 24 20 0F");
 
@@ -134,6 +134,14 @@ bool Initialize()
         cameraSpeedModifierA = (float*)(GameBase + 0x8EC2CC);
         cameraSpeedModifierB = (float*)(GameBase + 0x910F80);
         realTimeRate = (float*)(GameBase + 0x8F18E0);
+        break;
+    case 0x00010004:
+        GV_TimeBase = (int*)(GameBase + 0x1D97D10);
+        ActorWaitValue = (double*)(GameBase + 0x8F2DE8);
+        CutsceneFlag = (int*)(GameBase + 0x1172964);
+        cameraSpeedModifierA = (float*)(GameBase + 0x8F24B4);
+        cameraSpeedModifierB = (float*)(GameBase + 0x9186A0);
+        realTimeRate = (float*)(GameBase + 0x8F90A0);
         break;
     default:
         return false;
